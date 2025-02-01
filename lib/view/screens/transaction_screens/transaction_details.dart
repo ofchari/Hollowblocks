c0e669515997cd271fc285f699c3610936d2f25f
ofchari <ofchari4@email.com>
2025-02-01 19:20:54 +0530

    reference

--- lib/view/screens/transaction_screens/transaction_details.dart
+++ lib/view/screens/transaction_screens/transaction_details.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:vetri_hollowblock/view/screens/transaction_screens/payment_in.dart';
 import 'package:vetri_hollowblock/view/screens/transaction_screens/payment_out.dart';
